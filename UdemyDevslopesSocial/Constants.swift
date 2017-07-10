834c4445293b4044083d6c539f7d94fde874c6a3
Lily Hofman <0xlilyhoffman@gmail.com>
2017-07-10 13:55:05 -0700

    custom fancy views

---
+++ UdemyDevslopesSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  UdemyDevslopesSocial
+//
+//  Created by Lily Hofman on 7/10/17.
+//  Copyright © 2017 Lily Hoffman. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ UdemyDevslopesSocial/FancyButton.swift
@@ -0,0 +1,23 @@
+//
+//  FancyButton.swift
+//  UdemyDevslopesSocial
+//
+//  Created by Lily Hofman on 7/10/17.
+//  Copyright © 2017 Lily Hoffman. All rights reserved.
+//
+
+import UIKit
+
+class FancyButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+        
+    }
+}
